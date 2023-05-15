175afb1bfe16e2cc565657942dd2d2915a736467
AndreXavierCX <andrexavier99864@gmail.com>
2023-05-15 20:28:33 -0300

    commit for gui

--- somaVetor.c
+++ somaVetor.c
@@ -1,32 +1,37 @@
 #include <omp.h>
 #include <stdio.h>
+#define posicoes 2048
 
 int main(int argc, char *argv[])
 {
-    float a[2048];
-    float b[2048];
-    float c[2048];
+    float a[posicoes];
+    float b[posicoes];
+    float c[posicoes];
 
     int i;
 
 #pragma omp parallel
-#pragma omp for
-    for (i = 0; i < 2048; i++)
     {
-        a[i] = 2;
-        b[i] = 3;
-    }
+//#pragma omp for
+        for (i = 0; i < posicoes; i++)
+        {
+            a[i] = 2;
+            b[i] = 3;
+        }
 
-#pragma omp for
-    for (i = 0; i < 2048; i++)
-    {
-        c[i] = a[i] + b[i];
-    }
+//#pragma omp for
+        for (i = 0; i < posicoes; i++)
+        {
+            c[i] = a[i] + b[i];
+        }
 
-#pragma omp for
-    for (i = 0; i < 2048; i++)
-    {
-        printf("C[%d]=%f \n", i, c[i]);
+#pragma omp for ordered
+        for (i = 0; i < 10; i++)
+        {
+            #pragma omp ordered
+            // printf("C[%d]=%f \n",i,c[i]);
+            printf("thread %d interação %d do loop valor C[%d]=%f \n", omp_get_thread_num(), i, i, c[i]);
+        }
     }
     return 0;
 }

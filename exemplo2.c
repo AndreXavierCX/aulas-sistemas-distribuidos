76ae3ab6f5568fc9c9abd0f925d8eeb8c6f2efd5
AndreXavierCX <andrexavier99864@gmail.com>
2023-05-15 22:28:06 -0300

    ajuste exemplo2

--- exemplo2.c
+++ exemplo2.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
     int i;
 
-#pragma omp parallel shared(aux)
+#pragma omp parallel private(i) shared(aux, c)
     {
 #pragma omp for
         for (i = 0; i < posicoes; i++)
